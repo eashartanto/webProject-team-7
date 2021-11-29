0e6b677f9b46d83f1680c24707dbb39f20cb1644
nayakswaroop16 <83548441+nayakswaroop16@users.noreply.github.com>
2021-11-29 03:32:02 -0500

    comments

--- js/index.js
+++ js/index.js
@@ -1,15 +1,16 @@
 window.onload = function () {
+  //validate email on subscribe button click
   document.getElementById("subscribe").onclick = validateEmail;
 };
 
 let validateEmail = () => {
-  let emailRegex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
-  let $email = document.querySelector("#email");
+  let emailRegex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/; //regular expression to validate email
+  let $email = document.querySelector("#email");  //store email field in variable
   let email = $email.value;
-  if (email == "") {
+  if (email == "") { //if email is left empty
     $email.classList.add("error");
     $email.nextElementSibling.innerHTML = "Please enter an email address!";
-  } else if (!email.match(emailRegex)) {
+  } else if (!email.match(emailRegex)) { //if email is invalid
     $email.classList.add("error");
     $email.nextElementSibling.innerHTML = "Please enter a valid email address!";
   } else {
