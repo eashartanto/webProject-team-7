7f14ef57f457d8f0cfe916764ee53ef9505ab2d7
Aleksandr Mikhailov <alex01236@gmail.com>
2021-11-30 14:34:47 +0300

    Added index, search and raccoon article

---
+++ js/search.js
@@ -0,0 +1,42 @@
+import ARTICLES from "/js/articles.js";
+
+const resultsDiv = document.getElementById("search-results");
+
+function createElement(article) {
+    const newElement = document.createElement("div");
+    newElement.className = "search-result";
+
+    const img = document.createElement("img");
+    img.src = article.imageUrl;
+    newElement.appendChild(img);
+
+    const descDiv = document.createElement("div");
+    newElement.appendChild(descDiv);
+
+    const nameParagraph = document.createElement("h3");
+    nameParagraph.innerText = article.name;
+    descDiv.appendChild(nameParagraph);
+
+    const contentsParagraph = document.createElement("p");
+    contentsParagraph.innerText = article.contents.split(".")[0];
+    descDiv.appendChild(contentsParagraph);
+
+    return newElement;
+}
+
+function updateList() {
+    // Immitating backend query handling
+    const currentUrl = new URL(window.location.href);
+    const searchQuery = currentUrl.searchParams.get("q");
+    document.getElementById("search").value = searchQuery;
+    let fittingArticles = [];
+    ARTICLES.forEach(article => {
+        if (article.name.toLowerCase().includes(searchQuery) || article.contents.toLowerCase().includes(searchQuery)) {
+            fittingArticles.push(article);
+        }
+    });
+    // Processing the result
+    fittingArticles.forEach(article => resultsDiv.appendChild(createElement(article)));
+}
+
+updateList();
