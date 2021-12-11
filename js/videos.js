91424835b9c37ff3e97f3706e4eaa765aa5e1356
nayakswaroop16 <83548441+nayakswaroop16@users.noreply.github.com>
2021-12-11 15:10:49 -0500

    added json for videos.html

--- js/videos.js
+++ js/videos.js
@@ -1,3 +1,55 @@
+let videoList = [
+  {
+    src: "https://www.youtube.com/embed/UtPprUWteeo",
+    type: "Eat Me",
+    title: "Baker Rae Cafe is Toronto's go-to spot for cinnamon buns",
+  },
+  {
+    src: "https://www.youtube.com/embed/PNqcAycQ7bk",
+    type: "Happenings",
+    title: "Beyond Monet makes its world premiere in Toronto",
+  },
+  {
+    src: "https://www.youtube.com/embed/mm1cEFAuLb4",
+    type: "News",
+    title:
+      "This is how restaurants are keeping dine-in as safe as possible in Toronto",
+  },
+  {
+    src: "https://www.youtube.com/embed/9HsGB5KKksQ",
+    type: "Hot Spots",
+    title:
+      "TreaJah Isle is Toronto's world famous reggae record and convenience store",
+  },
+  {
+    src: "https://www.youtube.com/embed/fShxDCzwwbc",
+    type: "Hot Spots",
+    title:
+      "First Markham Place is one of the most iconic shopping plazas just outside Toronto",
+  },
+  {
+    src: "https://www.youtube.com/embed/Em4rquCVq2Q",
+    type: "Eat Me",
+    title:
+      "Yogurt House in Toronto serves up Balkan-style drinks and sandwiches",
+  },
+  {
+    src: "https://www.youtube.com/embed/ZjsUYuuqvXA",
+    type: "Hot Spots",
+    title: "Little Canada in Toronto",
+  },
+  {
+    src: "https://www.youtube.com/embed/wHKOaDvF2BA",
+    type: "News",
+    title: "Take a look inside the new Eglinton LRT vehiclesw",
+  },
+  {
+    src: "https://www.youtube.com/embed/Sj_np7mrntk",
+    type: "News",
+    title: "Deanne Hupfield teaches Powwow dancing with her online classes",
+  },
+];
+
 let filterVideos = (value) => {
   if (value == "all") {
     $(".card").show();
@@ -9,3 +61,19 @@ let filterVideos = (value) => {
     });
   }
 };
+
+let displayVideoCards = (vList) => {
+  videoCard = "";
+  vList.forEach((val) => {
+    videoCard += `<div class="card">
+                    <iframe data-type="news" width="560" height="200" src="${val.src}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>                
+                    <article>
+                        <span class="tag">${val.type}</span>
+                        <h4 class="title">${val.title}</h3>
+                    </article>
+                </div>`;
+  });
+  $('.card-list').html(videoCard);
+};
+
+displayVideoCards(videoList);
